4b91fa865fc3c8351df480fe8a69db2e673669d6
Kairo0628 <112845598+Kairo0628@users.noreply.github.com>
2026-01-24 18:06:58 +0900

    Add: Binary Tree Traversal

--- 05_Tree/README.md
+++ 05_Tree/README.md
@@ -1,7 +1,13 @@
 # 트리
 [노션 정리 링크]()
 
-### 
+### 2026-01-23
+- **binary_tree.py**
+    > size / depth
 
+### 2026-01-24
+- **binary_tree.py**
+    - 전위, 중위, 후위 순회
+    > in-order / pre-order / post-order
 
 ## 추가할 사항
--- 05_Tree/binary_tree.py
+++ 05_Tree/binary_tree.py
@@ -29,6 +29,33 @@ class Node():
             r = 0
 
         return max(l, r) + 1
+    
+    def in_order(self):
+        if self.left != None:
+            self.left.in_order()
+
+        print(self.data)
+
+        if self.right != None:
+            self.right.in_order()
+
+    def pre_order(self):
+        print(self.data)
+
+        if self.left != None:
+            self.left.pre_order()
+        
+        if self.right != None:
+            self.right.pre_order()
+
+    def post_order(self):
+        if self.left != None:
+            self.left.post_order()
+        
+        if self.right != None:
+            self.right.post_order()
+
+        print(self.data)
 
 
 class BinaryTree():
@@ -40,26 +67,43 @@ class BinaryTree():
     
     def depth(self):
         return self.root.depth()
-
+    
+    def in_order(self):
+        print('in-order traversal start')
+        return self.root.in_order()
+    
+    def pre_order(self):
+        print('pre-order traversal start')
+        return self.root.pre_order()
+    
+    def post_order(self):
+        print('post-order traversal start')
+        return self.root.post_order()
 
 x1 = Node(5)
 x2 = Node(17)
 x3 = Node(23)
 x4 = Node(44)
+x5 = Node(36)
+x6 = Node(67)
 
 tree = BinaryTree(x1)
 x1.left = x2
 x2.left = x3
 x2.right = x4
+x1.right = x5
+x5.right = x6
 
-print(tree.size())
-
-print(tree.depth())
-
+# structure
+#             5
+#         /      \
+#       17        36
+#    /      \         \
+#  23       44         67
 #
-#           x1
-#         /
-#        x2 
-#      /    \
-#     x3    x4
-#
+
+print(f'tree size: {tree.size()}')
+print(f'tree depth: {tree.depth()}')
+tree.in_order()
+tree.pre_order()
+tree.post_order()
